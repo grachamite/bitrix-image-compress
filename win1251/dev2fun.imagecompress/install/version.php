d3fb9660a26143d206388f5d597f29287d4caed3
grachamite <grachamite@hotmail.com>
2025-07-19 16:47:18 +0300

    Удалена версия в кодировке cp1251.
    - Удалена версия в кодировке cp1251;
    - Версия в utf-8 перенесена в корень проекта.

--- win1251/dev2fun.imagecompress/install/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-$arModuleVersion = [
-    'VERSION' => '0.11.7',
-    'VERSION_DATE' => '2025-06-26 00:25:00',
-];
--- win1251/dev2fun.imagecompress/lang/ru/classes/general/ImageCompressImagesToConvertedTable.php
+++
@@ -1,2 +0,0 @@
-<?php
-$MESS["DEV2FUN_IMAGECOMPRESS_REFERENCE_TABLE_TITLE"] = "Связь обычной картинки с кэшируемой";
